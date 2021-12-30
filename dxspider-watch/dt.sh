3e62a6f4de47bb8ad70b184d0f6adf79d1da704a
EA3CV <67219942+EA3CV@users.noreply.github.com>
2021-12-30 16:56:50 +0100

    FIX paths

--- dxspider-watch/dt.sh
+++ dxspider-watch/dt.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-source /root/spider-watch/157.ver
+source /root/dxspider-watch/157.ver
 
-cd /root/spider-watch/spider
+cd /root/dxspider-watch/spider
 
 git checkout mojo
 git pull
 GITVERSION=$(git describe --long | cut -d'-' -f2)
 
-cd /root/spider-watch
+cd /root/dxspider-watch
 
 if [[ $VERSION = $GITVERSION ]]
 then
     echo "No hay nueva version en Mojo"
 else
     echo "Hay nueva version en Mojo"
-    cd /root/spider-watch/mojo
+    cd /root/dxspider-watch/mojo
     docker build --no-cache -t dxspider:v157r$GITVERSION .
-    cd /root/spider-watch/spider
-    git describe --long | cut -d'-' -f2 | xargs -I {} echo "VERSION="{} > /root/spider-watch/157.ver
-    cd /root/spider-watch
+    cd /root/dxspider-watch/spider
+    git describe --long | cut -d'-' -f2 | xargs -I {} echo "VERSION="{} > /root/dxspider-watch/157.ver
+    cd /root/dxspider-watch
 fi
