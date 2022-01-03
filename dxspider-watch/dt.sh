14e5a9eaaa0b101cc38e5713986baeb2cc772ae8
EA3CV <67219942+EA3CV@users.noreply.github.com>
2022-01-03 15:01:31 +0100

    Update dt.sh

--- dxspider-watch/dt.sh
+++ dxspider-watch/dt.sh
@@ -3,17 +3,21 @@ source /root/dxspider-watch/157.ver
 
 cd /root/dxspider-watch/spider
 
-git checkout mojo
-git pull
+echo 'Última versión: '$VERSION | ts '[%Y-%m-%d %H:%M:%S]'
+
+git checkout mojo | ts '[%Y-%m-%d %H:%M:%S]'
+
+git pull | ts '[%Y-%m-%d %H:%M:%S]'
+
 GITVERSION=$(git describe --long | cut -d'-' -f2)
 
-cd /root/dxspider-watch
+echo 'Versión en repositorio: '$GITVERSION | ts '[%Y-%m-%d %H:%M:%S]'
 
 if [[ $VERSION = $GITVERSION ]]
 then
-    echo "No hay nueva version en Mojo"
+    echo "No hay nueva release en Mojo." | ts '[%Y-%m-%d %H:%M:%S]'
 else
-    echo "Hay nueva version en Mojo"
+    echo "Detectada nueva Release de Mojo" | ts '[%Y-%m-%d %H:%M:%S]'
     cd /root/dxspider-watch/mojo
     docker build --no-cache -t dxspider:v157r$GITVERSION .
     cd /root/dxspider-watch/spider
